66767a52a159f1abcaf66d2f123c19f0d941f4f3
Andy Chong Chin Shin <andychong2@gmail.com>
2016-01-30 19:51:54 +0800

    graph for convolution neural network done

--- src/common_functions.py
+++ src/common_functions.py
@@ -29,20 +29,27 @@ def accuracy(predictions, labels):
   return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) / predictions.shape[0])
 
 
-def get_train_valid_data(train_subset=45000, valid_size=5000):
+def get_train_valid_data(train_subset=45000, valid_size=5000, reformat_data=True, reformat_label=True):
   if train_subset + valid_size > 50000:
     raise Exception('train_subset + valid_size cannot be more than 50000')
   elif train_subset < 128:
     raise Exception('train_subset cannot be less than 128 (the batch size)')
 
   train_dataset, train_labels = load_train_data()
-  train_dataset = reformat_dataset(train_dataset)
-  train_labels = reformat_labels(train_labels)
+
+  if reformat_data:
+    train_dataset = reformat_dataset(train_dataset)
+
+  if reformat_label:
+    train_labels = reformat_labels(train_labels)
+
+  train_dataset = train_dataset.astype(np.float32)
+  train_labels = train_labels.astype(np.float32)
 
   # Create a validation dataset
-  valid_dataset = train_dataset[:valid_size, :]
+  valid_dataset = train_dataset[:valid_size]
   valid_labels = train_labels[:valid_size]
-  train_dataset = train_dataset[valid_size:valid_size + train_subset, :]
+  train_dataset = train_dataset[valid_size:valid_size + train_subset]
   train_labels = train_labels[valid_size:valid_size + train_subset]
   print 'Training set size:', train_dataset.shape, train_labels.shape
   print 'Validation set size:', valid_dataset.shape, valid_labels.shape
@@ -55,8 +62,8 @@ def reformat_dataset(dataset):
   # (50000, 32, 32, 3)
   # (50000, 3, 32, 32)
   dataset = np.rollaxis(dataset, 3, 1)
-  return dataset.reshape((-1, image_size * image_size * color_channel)).astype(np.float32)
+  return dataset.reshape((-1, image_size * image_size * color_channel))
 
 
 def reformat_labels(labels):
-  return (np.arange(num_labels) == labels[:, None]).astype(np.float32)
+  return (np.arange(num_labels) == labels[:, None])
--- src/convolution.py
+++ src/convolution.py
@@ -1,11 +1,74 @@
-from common_functions import get_train_valid_data
+from common_functions import get_train_valid_data, accuracy
+from constants import image_size, color_channel, num_labels
+import tensorflow as tf
+
+
+def weight_variable(shape):
+  return tf.Variable(tf.truncated_normal(shape, stddev=0.1))
+
+
+def bias_variable(shape):
+  return tf.Variable(tf.zeros(shape))
+
+
+def constant_bias_variable(constant, shape):
+  return tf.Variable(tf.constant(constant, shape=shape))
 
 
 def run_convolution(train_subset=45000, valid_size=5000, test=False):
   train_dataset, train_labels, valid_dataset, valid_labels = \
-      get_train_valid_data(train_subset, valid_size)
+      get_train_valid_data(train_subset, valid_size, reformat_datag=False)
 
   print 'Building graph...'
+
+  batch_size = 16
+  patch_size = 5
+  depth = 16
+  num_hidden = 64
+
+  graph = tf.Graph()
+  with graph.as_default():
+    # Input data.
+    tf_train_dataset = tf.placeholder(
+        tf.float32, shape=(batch_size, image_size, image_size, color_channel))
+    tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+    tf_valid_dataset = tf.constant(valid_dataset)
+    tf_valid_labels = tf.constant(valid_labels)
+
+    # Variables.
+    layer1_weights = weight_variable([patch_size, patch_size, color_channel, depth])
+    layer1_biases = bias_variable(shape=[depth])
+    layer2_weights = weight_variable([patch_size, patch_size, depth, depth])
+    layer2_biases = constant_bias_variable(1.0, [depth])
+    layer3_weights = weight_variable([image_size / 4 * image_size / 4 * depth, num_hidden])
+    layer3_biases = constant_bias_variable(1.0, [num_hidden])
+    layer4_weights = weight_variable([num_hidden, num_labels])
+    layer4_biases = constant_bias_variable(1.0, [num_labels])
+
+    # Model.
+    def model(data):
+      # Convolution layers
+      conv = tf.nn.conv2d(data, layer1_weights, [1, 2, 2, 1], padding='SAME')
+      hidden = tf.nn.relu(conv + layer1_biases)
+      conv = tf.nn.conv2d(hidden, layer2_weights, [1, 2, 2, 1], padding='SAME')
+      hidden = tf.nn.relu(conv + layer2_biases)
+
+      # MLP layers
+      shape = hidden.get_shape().as_list()
+      reshape = tf.reshape(hidden, [shape[0], shape[1] * shape[2] * shape[3]])
+      hidden = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
+      result = tf.matmul(hidden, layer4_weights) + layer4_biases
+      return result
+
+    train_logits = model(tf_train_dataset)
+    train_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(train_logits, tf_train_labels))
+    train_prediction = tf.nn.softmax(train_logits)
+
+    optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(train_loss)
+
+    valid_logits = model(tf_valid_dataset)
+    valid_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(valid_logits, tf_valid_labels))
+    valid_prediction = tf.nn.softmax(valid_logits)
 
 
 if __name__ == '__main__':
